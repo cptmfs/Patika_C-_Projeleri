971ac1e2a6c71cf805942acd99a2e9a82b708246
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:27:09 +0300

    Karakter_Tersten_Yazdırma

---
+++ Karakter_Tersten_Yazdırma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("İfade giriniz:");
+        string ifade = Console.ReadLine();
+        string result = "";
+
+        for (int i = ifade.Length - 1; i >= 0; i--)
+        {
+            result += ifade[i];
+        }
+        Console.WriteLine("Sonuç: " + result);
+        Console.ReadKey();
+    }
+
+    static string SwapCharactersWithPrevious(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 1; i < charArray.Length; i ++) // 
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[i - 1];
+            charArray[i - 1] = temp;
+        }
+
+        return new string(charArray);
+    }
+}
