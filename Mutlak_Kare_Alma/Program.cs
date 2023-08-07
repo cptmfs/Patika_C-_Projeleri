7b98990105ac11806247ce8a5d2c57f4683de883
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:37:51 +0300

    Mutlak_Kare_Alma

---
+++ Mutlak_Kare_Alma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Sayıları girin (örn. '56 45 68 77'): ");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int smallerSum = 0; //67
+        int biggerSum = 0; //67
+
+        foreach (string num in numbers)
+        {
+            int currentNum = int.Parse(num);
+
+            if (currentNum < 67)
+            {
+                smallerSum += 67 - currentNum;
+            }
+            else
+            {
+                biggerSum += Math.Abs(67 - currentNum) * Math.Abs(67 - currentNum);
+            }
+        }
+
+        Console.WriteLine($"Sonuç: {smallerSum} {biggerSum}");
+    }
+}
