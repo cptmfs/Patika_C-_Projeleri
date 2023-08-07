ac0cb6f61297aa1de384899be96263a249c63e85
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:42:06 +0300

    Karakter_Degistirme

---
+++ Karakter_Degistirme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a text : ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+        string[] newWords = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            char firstChar = words[i][0];
+            char lastChar = words[i][words[i].Length - 1];
+
+            string newWord = lastChar + words[i].Substring(1, words[i].Length - 2) + firstChar;
+            newWords[i] = newWord;
+
+        }
+
+        string output = string.Join(" ", newWords);
+        Console.WriteLine($"Yeni metin: {output}");
+        Console.ReadKey();  
+    }
+
+}
