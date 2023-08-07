be855ec27170498e740e3e9d35ca6b63a5bcf5b8
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:34:25 +0300

    Integer_İkililerin_Toplamı

---
+++ Integer_İkililerin_Toplamı/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Integer ikilileri girin (örn. '2 3 1 5'): ");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        List<int> results = new List<int>();
+
+        for (int i = 0; i < numbers.Length; i += 2)
+        {
+            int num1 = int.Parse(numbers[i]);
+            int num2 = int.Parse(numbers[i + 1]);
+
+            if (num1 != num2)
+            {
+                results.Add(num1 + num2);
+            }
+            else
+            {
+                results.Add(num1 * num1);
+            }
+        }
+
+        Console.WriteLine("Sonuç: " + string.Join(" ", results));
+    }
+}
