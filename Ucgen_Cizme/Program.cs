8e67f8d0a5a92a206f5c4a7a945c5dd199fb7950
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:18:24 +0300

    Ucgen_Cizme

---
+++ Ucgen_Cizme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class UcgenCiz
+{
+    public void DrawTriangle(int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            for (int j = 1; j <= size - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Üçgenin boyutunu giriniz: ");
+        int size = int.Parse(Console.ReadLine());
+
+        UcgenCiz ucgenCiz = new UcgenCiz();
+        ucgenCiz.DrawTriangle(size);
+    }
+}
