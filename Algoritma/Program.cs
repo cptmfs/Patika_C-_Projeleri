d6ac18dd0b35b25fffd4b95d1f2aecd5063b64fa
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-08 02:22:58 +0300

    Algoritma

---
+++ Algoritma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir string ifade ve bir sayı giriniz (örn. Algoritma,3): ");
+        string input = Console.ReadLine();
+        string[] inputParts = input.Split(',');
+
+        if (inputParts.Length != 2)
+        {
+            Console.WriteLine("Hatalı giriş! Lütfen doğru formatta giriş yapın.");
+            return;
+        }
+
+        string text = inputParts[0].Trim();
+        if (!int.TryParse(inputParts[1].Trim(), out int index) || index < 0 || index >= text.Length)
+        {
+            Console.WriteLine("Geçersiz indeks! Lütfen geçerli bir indeks girin.");
+            return;
+        }
+
+        string result = RemoveCharacterAtIndex(text, index);
+        Console.WriteLine($"Sonuç: {result}");
+    }
+
+    static string RemoveCharacterAtIndex(string text, int index)
+    {
+        return text.Remove(index, 1);
+    }
+}
